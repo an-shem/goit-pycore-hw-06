115603970482778fbca0c839c1bc5a850609ba38
Andrey Shemet <andrey.shemet.nik@gmail.com>
2025-11-12 00:15:18 +0100

    I removed all the prints and replaced them with error forwarding. I moved the errors to a separate class.

--- task_1.py
+++ task_1.py
@@ -1,10 +1,11 @@
 from collections import UserDict
-
-
-class InvalidNumberError(Exception):
-    """Ошибка: номер некорректный."""
-
-    pass
+from error import (
+    ContactNotFoundError,
+    DuplicatePhoneNumberError,
+    InvalidContactError,
+    InvalidNumberError,
+    PhoneNumberNotFoundError,
+)
 
 
 class Field:
@@ -16,7 +17,11 @@ class Field:
 
 
 class Name(Field):
-    pass
+    def __init__(self, name):
+        if type(name) != str:
+            raise InvalidContactError(f"Name must be a string")
+        name = name.strip()
+        super().__init__(name)
 
 
 class Phone(Field):
@@ -31,7 +36,10 @@ class Phone(Field):
 
 class Record:
     def __init__(self, name: str):
-        self.name = Name(name)
+        try:
+            self.name = Name(name)
+        except InvalidContactError:
+            raise
         self.phones = []
 
     def __str__(self):
@@ -42,104 +50,98 @@ class Record:
             ph = Phone(phone)
             for p in self.phones:
                 if p.value == phone:
-                    print(
+                    raise DuplicatePhoneNumberError(
                         f"Contact {self.name} already has the telephone number {phone}."
                     )
-                    return
             self.phones.append(ph)
-        except InvalidNumberError as e:
-            print("Error:", e)
-        else:
-            print(
-                f"The phone number {phone} has been added to the contact {self.name}."
-            )
+        except (InvalidNumberError, DuplicatePhoneNumberError):
+            raise
+        except Exception:
+            raise
 
     def edit_phone(self, old_phone, new_phone):
         if not old_phone or not new_phone or old_phone == new_phone:
-            print("Phone numbers entered incorrectly.")
-            return 1
+            raise InvalidNumberError("Phone numbers entered incorrectly.")
         [index] = [i for i, p in enumerate(self.phones) if p.value == old_phone]
         self.phones[index] = Phone(new_phone)
-        print("Phone number updated.")
 
     def find_phone(self, phone):
         if not phone:
-            print("Phone numbers entered incorrectly.")
-            return
+            raise InvalidNumberError("Phone numbers entered incorrectly.")
         for p in self.phones:
             if p.value == phone:
-                print("Phone number found.")
                 return p.value
-        print("Phone number not found.")
-        return None
+        raise PhoneNumberNotFoundError("Phone number not found.")
 
     def remove_phone(self, phone):
         if not phone or len(phone) != 10:
-            print("Phone numbers entered incorrectly.")
-            return
+            raise InvalidNumberError("Phone numbers entered incorrectly.")
         [index] = [i for i, p in enumerate(self.phones) if p.value == phone]
         del self.phones[index]
-        print("Phone number deleted.")
         return phone
 
 
 class AddressBook(UserDict):
     def add_record(self, record: Record):
         if not isinstance(record, Record):
-            print("In AddressBook, you can append only an object of the Record class.")
-            return
+            raise ValueError(
+                "In AddressBook, you can append only an object of the Record class."
+            )
         name_key = record.name.value
         if not name_key:
-            print("You cannot add a contact without a name to AddressBook.")
-            return
+            raise ValueError("You cannot add a contact without a name to AddressBook.")
         if name_key in self.data:
-            print("Contact with that name already exists.")
-            return
+            raise DuplicatePhoneNumberError("Contact with that name already exists.")
         self.data[name_key] = record
         return self.data[name_key]
 
     def find(self, name):
         if not name in self.data:
-            print("Contact with this name not found.")
-            return None
+            raise ContactNotFoundError("Contact with this name not found.")
         return self.data[name]
 
     def delete(self, name):
         if not name:
-            print("Name entered incorrectly.")
-            return
+            raise InvalidContactError("Name entered incorrectly.")
         if name in self.data:
             contact = self.data[name]
             del self.data[name]
-            print(f"Contact named {name} deleted.")
             return contact
 
 
-# Створення нової адресної книги
-book = AddressBook()
-# Створення запису для John
-john_record = Record("John")
-john_record.add_phone("1234567890")
-john_record.add_phone("5555555555")
-# # Додавання запису John до адресної книги
-book.add_record(john_record)
-# # Створення та додавання нового запису для Jane
-jane_record = Record("Jane")
-jane_record.add_phone("9876543210")
-book.add_record(jane_record)
-# # Виведення всіх записів у книзі
-for name, record in book.data.items():
-    print(record)
-# # Знаходження та редагування телефону для John
-john = book.find("John")
-john.edit_phone("1234567890", "1112223333")
-print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
-# # Пошук конкретного телефону в записі John
-found_phone = john.find_phone("5555555555")
-print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
-# # Видалення запису Jane
-book.delete("Jane")
-john.remove_phone("1112223333")
+def main():
+    try:
+        # Створення нової адресної книги
+        book = AddressBook()
+        # Створення запису для John
+        john_record = Record("John")
+        john_record.add_phone("1234567890")
+        john_record.add_phone("5555555555")
+        # # Додавання запису John до адресної книги
+        book.add_record(john_record)
+        # # Створення та додавання нового запису для Jane
+        jane_record = Record("Jane")
+        jane_record.add_phone("9876543210")
+        book.add_record(jane_record)
+        # # Виведення всіх записів у книзі
+        for name, record in book.data.items():
+            print(record)
+        # # Знаходження та редагування телефону для John
+        john = book.find("John")
+        john.edit_phone("1234567890", "1112223333")
+        print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+        # # Пошук конкретного телефону в записі John
+        found_phone = john.find_phone("5555555555")
+        print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+        # # Видалення запису Jane
+        book.delete("Jane")
+        john.remove_phone("1112223333")
 
-for name, record in book.data.items():
-    print(record)
+        for _, record in book.data.items():
+            print(record)
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
